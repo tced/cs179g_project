37cd4b32bda1e82ca2bade1f49d6d91f8039f679
Andrea Cruz Castillo <castillo_cruza@hotmail.com>
2018-12-10 15:26:57 -0800

    removing duplicate web interface files

--- search/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-
-#delete posts, create users 
-# Register your models here.
--- search/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.template import loader 
-from django.http import HttpResponse
-
-# Create your views here.
-#takes the request, and sends back an HTTP response 
-def index(request): 
-	template = loader.get_template('search/homepage.html')
-	return HttpResponse(template.render())  
